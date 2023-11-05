fc6cb6eab129d8691e1bf68be5964cbe49fcd022
Numaku <manhcuong240902@gmail.com>
2023-11-05 20:51:47 +0700

    code phần điều hướng màn hình

--- app/src/main/java/com/example/androidmusicapp/activity/UserProfileActivity.java
+++ app/src/main/java/com/example/androidmusicapp/activity/UserProfileActivity.java
@@ -3,14 +3,26 @@ package com.example.androidmusicapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.androidmusicapp.R;
+import com.example.androidmusicapp.api.ApiService;
+import com.example.androidmusicapp.model.entity.Song;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserProfileActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
     }
+
 }

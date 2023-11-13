229c1834fc20d04b1d583c6db9de78d9dd3c4df5
Numaku <manhcuong240902@gmail.com>
2023-11-13 12:36:17 +0700

    song display

--- app/src/main/java/com/example/androidmusicapp/api/ApiService.java
+++ app/src/main/java/com/example/androidmusicapp/api/ApiService.java
@@ -2,6 +2,7 @@ package com.example.androidmusicapp.api;
 
 
 
+import com.example.androidmusicapp.model.entity.Song;
 import com.example.androidmusicapp.model.entity.User;
 
 
@@ -10,12 +11,16 @@ import com.example.androidmusicapp.model.entity.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 
@@ -27,24 +32,24 @@ public interface ApiService {
             .create();
     ApiService apiService = new Retrofit.Builder()
             //ip máy
-
-            .baseUrl("http://192.168.39.30:8081/")
-
-       //     .baseUrl("http://192.168.1.5:8081/")
-            //192.168.1.5
-            //192.168.39.220
-
+            .baseUrl("http://192.168.39.220:8081/")
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiService.class);
-    @POST("auth/signin")
+    //     .baseUrl("http://192.168.1.5:8081/")
+    //192.168.1.5
+    //192.168.39.220
+    //192.168.1.13
 
-    Call<User> sendUser(@Body User user);
+    @POST("auth/signin")
 
 
     Call<User> signIn(@Body User user);
     @POST("auth/signup")
     Call<ResponseBody> signUp(@Body User user);
     //test
+
+    @GET("song")
+    Call<ArrayList<Song>> getSongs();
 }
 
--- app/src/main/java/com/example/androidmusicapp/fragment/HomeFragment.java
+++ app/src/main/java/com/example/androidmusicapp/fragment/HomeFragment.java
@@ -5,15 +5,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.androidmusicapp.R;
+import com.example.androidmusicapp.adapter.SongAdapter;
+import com.example.androidmusicapp.api.ApiService;
+import com.example.androidmusicapp.model.entity.Song;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
-
+    ApiService apiService;
+    RecyclerView recyclerView;
+    SongAdapter songAdapter;
+    private List<Song> songList;
 
 
     public static HomeFragment newInstance() {
@@ -23,9 +40,43 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        songList = new ArrayList<>();
+
+
+
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.SongDisplay);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        songAdapter = new SongAdapter(getActivity(), songList);
+        recyclerView.setAdapter(songAdapter);
+        loadSong();
+        return view;
+
     }
 
+    private void loadSong() {
+        ApiService.apiService.getSongs().enqueue(new Callback<ArrayList<Song>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Song>> call, Response<ArrayList<Song>> response) {
+                if (response.isSuccessful()) {
+                    // Update the existing songList
+                    songList.clear(); // Clear the previous data
+                    songList.addAll(response.body());
 
+                    // Notify the adapter that the data has changed
+                    songAdapter.notifyDataSetChanged();
+                } else {
+                    // Handle error
+                    Toast.makeText(getActivity(), "Text!", Toast.LENGTH_SHORT).show();
+                }
+            }
 
+            @Override
+            public void onFailure(Call<ArrayList<Song>> call, Throwable t) {
+                // Handle failure
+                Toast.makeText(getActivity(), "Fail!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
+

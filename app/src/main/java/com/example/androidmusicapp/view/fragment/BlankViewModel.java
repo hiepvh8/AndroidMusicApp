97f517ed50279c63e577ad4819aebdafc9b5a552
Numaku <manhcuong240902@gmail.com>
2023-12-22 20:14:27 +0700

    final version

--- app/src/main/java/com/example/androidmusicapp/view/fragment/BlankViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.androidmusicapp.view.fragment;
-
-import androidx.lifecycle.ViewModel;
-
-public class BlankViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
